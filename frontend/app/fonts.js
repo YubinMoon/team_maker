c0a2372dd6c2d8be01fed33432f916e7e224d855
terry <munu220@gmail.com>
2023-10-29 07:53:08 +0000

    폰트 추가

---
+++ frontend/app/fonts.js
@@ -0,0 +1,13 @@
+import { Do_Hyeon, Nanum_Gothic } from 'next/font/google'
+
+export const nanum_gothic = Nanum_Gothic({
+  weight: ['400', '700', '800'],
+  subsets: ['latin'],
+  display: 'swap'
+})
+
+export const do_hyeon = Do_Hyeon({
+  weight: ['400'],
+  subsets: ['latin'],
+  display: 'swap',
+})
